c8c8aacd8571f7bc96a73600b89816ccc3910844
mohammadtonor <mtonor1368@gmail.com>
2024-01-10 20:56:25 +0330

    landing page creating and nav menu items

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: 'ui_kits' as const,
+        featured: [
+            {
+                name: "Eitor picks",
+                href: '#',
+                imageSrc: '/nav/ui-kits/mixed.jpg'
+            },
+            {
+                name: "New Arivals",
+                href: '#',
+                imageSrc: '/nav/ui-kits/blue.jpg'
+            },
+            {
+                name: "Best Sallers",
+                href: '#',
+                imageSrc: '/nav/ui-kits/purple.jpg'
+            },
+        ]
+    },
+    {
+        label: "Icons",
+        Value: 'icons' as const,
+        featured: [
+            {
+                name: "favorites Icon Pickes",
+                href: '#',
+                imageSrc: '/nav/icons/picks.jpg'
+            },
+            {
+                name: "New Arivals",
+                href: '#',
+                imageSrc: '/nav/icons/new.jpg'
+            },
+            {
+                name: "Bestselling Icons",
+                href: '#',
+                imageSrc: '/nav/icons/bestsellers.jpg'
+            },
+        ]
+    }
+];
